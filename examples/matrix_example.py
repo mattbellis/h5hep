5c27c00e6db7afc64f7e1cd23f8d73ea62b6ab47
Matt Bellis <matthew.bellis@gmail.com>
2018-05-25 16:37:50 -0400

    Stuff.

--- examples/matrix_example.py
+++ examples/matrix_example.py
@@ -25,37 +25,34 @@ px_size = 2
 py_size = 5
 
 # Initialize the data dictionary for a single matrix
-matrix = hp.initialize()
+data = hp.initialize()
     
 #Create groups
-hp.create_group(matrix, "points", "npoints")
-hp.create_group(matrix, "data", "ndata")
+hp.create_group(data, "pixels", counter="npixels")
 
 # Create datasets within the groups
-hp.create_dataset(matrix, ["x","y"], "points")
-hp.create_dataset(matrix, ["data_point"], "data")
+hp.create_dataset(data, ["x","y","value"], group="pixels")
 
-# Create an event from the matrix framework
-event = hp.create_single_event(matrix)
+# Create an event from the data container
+event = hp.create_single_event(data)
 
 for i in range(0,nmat):
-    # Random selection of number of points in the matrix
+    # Random selection of number of pixels in the matrix
     r = np.random.randint(0,x_size*y_size)
 
     # For each point, randomly select an x and y position
     # Repeats are ok in this example
     for i in range(0,r):
-        event['points/x'].append(np.random.randint(1,x_size+1))
-        event['points/y'].append(np.random.randint(1,y_size+1))
-        event['data/data_point'].append(np.random.randint(1,70))
+        event['pixels/x'].append(np.random.randint(1,x_size+1))
+        event['pixels/y'].append(np.random.randint(1,y_size+1))
+        event['pixels/value'].append(np.random.randint(1,70))
 
     # Counters are the same here because there is always one data point per point
     # They could be different if there was more data collected for each point
-    event['data/ndata'] = r
-    event['points/npoints'] = r
+    event['pixels/npixels'] = r
     
     # Add the event to the matrix dictionary
-    hp.fill(matrix,event)
+    hp.fill(data,event)
 
     # Clear the event matrix for the next iteration
     hp.clear_event(event)
@@ -64,45 +61,39 @@ for i in range(0,nmat):
 # Write to file
 ###############
 filename = 'matrix_example_output.hdf5'
-hdfile = hp.write_to_file(filename,matrix,comp_type='gzip',comp_opts=9)
+hdfile = hp.write_to_file(filename,data,comp_type='gzip',comp_opts=9)
 
 ################
 # Read from file
 ################
 data,event = hp.hd5events(filename)
+nevents = data['nevents']
 print(data)
+print("nevents: ",nevents)
 #nevents = data['nevents']
 
+for i in range(0,nevents):
 
+    hp.get_event(event,data,n=i)
 
-####################################################
-# Plotting to check out what's inside the dictionary
-####################################################
+    print(event.keys())
 
+    npixels = event['pixels/npixels']
+    x = event['pixels/x']
+    y = event['pixels/y']
+    d = event['pixels/value']
 
+    grid = np.zeros((x_size,y_size))
 
-
-
-count = 0
-## WILL BE CHANGED WHEN READ IN FROM FILE
-for i in range(0,nmat):
-
-    npoints = matrix['points/npoints'][i]
-    x = matrix['points/x'][count:count+npoints]
-    y = matrix['points/y'][count:count+npoints]
-    d = matrix['data/data_point'][count:count+npoints]
-
-    data = np.zeros((x_size,y_size))
-
-    for i in range(npoints):
-        data[x[i]-1,y[i]-1] = d[i]
+    for i in range(npixels):
+        grid[x[i]-1,y[i]-1] = d[i]
 
     # create discrete colormap
     #cmap = colors.ListedColormap(['red', 'blue'])
     cmap = plt.get_cmap('plasma')
 
     fig, ax = plt.subplots()
-    ax.imshow(data, cmap=cmap)
+    ax.imshow(grid, cmap=cmap)
 
     # draw gridlines
     ax.grid(which='major', axis='both', linestyle='-', color='k', linewidth=2)
@@ -114,7 +105,7 @@ for i in range(0,nmat):
 
 
 
-#plt.show()
+plt.show()
 
 
 
