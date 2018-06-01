da2868ddde56ebce459191619dadd300ef90cfda
mattbellis <matthew.bellis@gmail.com>
2018-06-01 02:13:05 -0400

    Minimal changes. Some bug fixes.

--- examples/cylindrical_example.py
+++ examples/cylindrical_example.py
@@ -7,19 +7,20 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import colors
 
 # Number of layers
-nlayers = 1
+nlayers = 3
 
 # Number of events
-nevents = 9
+nevents = 4
 
 # Assign dimensions
-z_size = 10
+z_size = [20, 24, 28]
 
 # Max number of pixels in each layer
 # Assume i pixels per ten degrees
 maxpix = []
-for i in range(0,nlayers+1):
-    maxpix.append(z_size*(i+1)*36)
+for i in range(0,nlayers):
+    #maxpix.append(z_size[i]*(i+1)*36)
+    maxpix.append(z_size[i]*(i+1)*18)
 
 # Initialize the data dictionary for a single matrix
 data = hp.initialize()
@@ -34,21 +35,26 @@ hp.create_dataset(data, ["z","theta","r","value"], group="pixels")
 event = hp.create_single_event(data)
 
 for i in range(0,nevents):
-    # Random selection of number of hit pixels in the layer
+
+    npixtot = 0
+    ## Random selection of number of hit pixels in the layer
     for j in range(nlayers): 
         npix = np.random.randint(0,maxpix[j])
 
         # For each point, randomly select a z and theta
         # Repeats are ok in this example
         for i in range(0,npix):
-            event['pixels/z'].append(np.random.randint(0,z_size))
+            event['pixels/z'].append(np.random.randint(0,z_size[j]) - z_size[j]/2)
             event['pixels/r'].append(j)
             event['pixels/theta'].append(np.random.random()*2*np.pi)
             event['pixels/value'].append(np.random.randint(1,70))
 
+        # Keep a count of the total number of pixels
+        npixtot += npix
+
     # Counters are the same here because there is always one data point per point
     # They could be different if there was more data collected for each point
-    event['pixels/npixels'] = npix
+    event['pixels/npixels'] = npixtot
 
     # Add the event to the matrix dictionary
     hp.pack(data,event)
@@ -94,10 +100,11 @@ for i in range(0,nevents):
     zipped = zip(x,y,z,d)
     sorted(zipped, key=lambda g: g[2])
 
-    ax = fig.add_subplot(3, 3, i+1, projection='3d')
-    ax.scatter(x,y,z,c=d)
-    ax.plot_surface(x1,y1,z1)
-    ax.plot_surface(x1,y1,z1)
+    ax = fig.add_subplot(2, 2, i+1, projection='3d')
+    ax.scatter(x,y,z,c=d, alpha=0.2)
+    #ax.scatter(x,y,z,c=r, alpha=0.2)
+    #ax.plot_surface(x1,y1,z1)
+    #ax.plot_surface(x1,y1,z1)
     ax.set_zlim(0, 10)
 
 plt.show()
